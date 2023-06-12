5368c104c3b0201a2aec3415af4502b9d5fe8990
Aswathi <aswathi259@gmail.com>
2023-06-12 14:13:27 +0530

    Add. Added http methods using Node.js

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log('app.js running')
+const http = require('http');
+const PORT = 3500;
+
+const server = http.createServer((req,res) => {
+    const URL = req.url;
+    const METHOD = req.method;
+    res.setHeader('Content-Type', 'text/plain');
+
+    if(URL === '/'){
+    if(METHOD === 'GET'){
+        res.write('GET request');
+        res.end();
+    }
+
+    else if(METHOD === 'POST'){
+        res.write(`POST request`);
+        res.end();
+    }
+
+    else if(METHOD === 'PUT'){
+        res.write(`PUT request`);
+        res.end();
+    }
+
+    else if(METHOD === 'DELETE'){
+        res.write(`DELETE request`);
+        res.end();
+    }}
+    else{
+        res.write('Invalid url');
+        res.end();
+    }
+})
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
